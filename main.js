6343e82e51d2ea5c975db982ad5643ea2314b268
aman-can <aman.dubey1998@gmail.com>
2020-05-02 23:01:07 +0530

    fixed js

--- main.js
+++ main.js
@@ -2,16 +2,24 @@ function others(any) {
   var element = document.getElementById("other");
   if (any == "Other") {
     element.style.display = "block";
+    element.disabled = false;
     hideselect1();
-  } else element.style.display = "none";
+  } else {
+    element.style.display = "none";
+    element.disabled = true;
+  }
 }
 
 function specific_times(any) {
   var element = document.getElementById("specific_time");
   if (any == "some specific time") {
     element.style.display = "block";
+    element.disabled = false;
     hideselect();
-  } else element.style.display = "none";
+  } else {
+    element.style.display = "none";
+    element.disabled = true;
+  }
 }
 
 function hideselect1() {
@@ -28,4 +36,4 @@ window.setTimeout(function () {
   alert(
     "This form does not get any data to me ,so just don't type anything and press submit.If you really want to show this to me then send me a screenshot of this form filled."
   );
-}, 100);
+}, 200);
